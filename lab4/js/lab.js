ee7a23dcebcefbc7df17e19a4a321c485fecd957
mestie <meelthom@ucsc.edu>
2025-04-20 17:22:44 -0700

    Lab 4 -final

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 brushing teeth and Pong game pseudocoding. 
+// Author: Mestie Thomas
+// Date:4/20/25
 
 // Constants
 
@@ -19,3 +19,16 @@ function main() {
 
 // let's get this party started
 main();
+// Brushing teeth task
+  // Go to the bathroom
+	// Pick up toothpast and uncap it
+	//  Squeeze toothpaste onto toothbrush
+	// Brush teeth for 2 minutes 
+
+  // Pong game task 
+    // Start game
+	  // Wait for ball to bounce off wall
+		// Move paddle up or down to hit ball
+		// If ball hits paddle, bounce it back
+		// If ball misses paddle, lose a point
+		// Repeat until game is over
