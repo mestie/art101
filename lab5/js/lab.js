6190566a842188c7960417f92740f372b8c37cd8
mestie <meelthom@ucsc.edu>
2025-04-24 15:11:02 -0700

    Tiral lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5. Experimenting with basic JavaScript data types and variables.
+// Author: Mestie Thomas
+// Date: 04/24/25
 
-// Constants
+// define variables 
+let make = "Toyota";
+let model = "Corolla";
+let color = "grey";
+let year = 2018;
+let age = 2025 - year; 
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//outpput 
+document.writeln("Make; " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>"); 
