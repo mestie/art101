3e29ab6da2747e2797e8fd330a8e563cd9d5bd43
mestie <meelthom@ucsc.edu>
2025-04-29 15:19:10 -0700

    lab 6 trial run

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
+// index.js - Lab 6
+// Author: mestie
 // Date:
 
-// Constants
+//lab assignment my transportaion 
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+var myMainRide = { 
+  color: "grey", 
+  year: 2018, 
+  age: function() {
+      return 2025 - this.year;
+  } 
+} 
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>"); 
