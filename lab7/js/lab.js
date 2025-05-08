655eefbadfcb6ae4dc01a0541351b33af740e989
mestie <meelthom@ucsc.edu>
2025-05-07 21:09:59 -0700

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,11 +1,11 @@
 // index.js - Lab 7
 // Author: mestie
-// Date:5.4.2025
+// Date: 5.4.2025
 //ger user input 
 function myVariable () {
     var userName = window.prompt("Who are you?");
     console.log("userName =", userName);
     return userName.toLowerCase().split("").sort().join("");
-} 
-// ouutpuut 
-document.writelm("Who are you, ", myVariable (), "<br>", "Nice to meet you!");
+}
+// output
+document.writeln("Who are you, ", myVariable(), "<br>", "Nice to meet you!");
