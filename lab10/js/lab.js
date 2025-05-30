978d63a43e0b44f9026e8653e0b4ca48d9936520
mestie <meelthom@ucsc.edu>
2025-05-30 13:17:06 -0700

    lab 10 edits

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,20 +1,27 @@
 // index.js - Lab 10
 // Author: mestie
-// Date: 6.28.2025
+// Date: 6.30.2025
 
 function generateRandomText() {
-    const text = "wjuhgtis jsi8utj3-newe";
+    const text = "ሰላም የሆነ ነገር ተናገር";
     const min = 3; 
     const max = 100; 
     const randlen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // get a random start index to slice the wjuhgtis jsi8utj3-newe text 
+    // get a random start index to slice the ሰላም የሆነ ነገር ተናገር
     const randStart = Math.floor(Math.random() * (text.length - randlen + 1));
-    // generate the random wjuhgtis jsi8utj3-newe like text 
+    // generate the random ሰላም የሆነ ነገር ተናገር like text 
     return text.slice(randStart, randStart + randlen);
 }
  
 // click listener for button 
-$("#make-convo").click(function(){
-})
+$("#start-convo").click(function(){
+});
 
-// click 
+// click listener for button 
+$("#start-convo").click(function() {
+    // get the random text 
+    const mewText = generateRandomText();
+
+// append a new div to our ouput div
+$("#output").append('<div class="text"><p>' + mewText + '</p></div>');
+});
