fe577e3662b6bf3fcc9e9fa9c1023ab0ed348894
mestie <meelthom@ucsc.edu>
2025-06-10 11:03:21 -0700

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,29 +1,77 @@
 // index.js - Lab 8
 // Author: mestie
-// Date: 5.8.2025
+// Date: 7.10.2025
+maxFactors = 4;
 
+outputEl = document.getElementById("output");
 
-function myNumbers(x){
-    return (x % 2== 0); 
+// get the values from the webpage and write them in an object
+
+function getFactorObj() {
+    var factorObj = {};
+    for (var factor=0; factor<maxFactors; factor++) {
+        numId = "num" + factor;
+        textId = "text" + factor;
+        numValue = document.getElementById(numId).value;
+        textValue = document.getElementById(textId).value;
+        console.log(factor + ") num:", numValue, "text:", textValue)
+        // if either value is blank, don't use it
+        if (numValue && textValue) {
+            factorObj[numValue] = textValue;
+        }
+    }
+    return factorObj;
 }
-// test function 
-console.log("Is 1 even? " + myNumbers(1)); 
-console.log("Is 2 even? " + myNumbers(2));
 
-array = [500, 7, 34, 50] 
-console.log("My array", array); 
+function outputToPage(str) {
+    newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    outputEl.appendChild(newEl);
+}
 
-var result = array.map(myNumbers);
-// should return [true, false, true, true] 
-console.log("Test of evenness of array;", result); 
+// loops over the numbers and outputs the number and the matching text
+function fizzBuzzBoom(maxNums, factorObj) {
 
-var result = array.map(function(x){
-    return x ** 0.5; 
-}) 
-// should return [22.36, 2.65, 5.83, 7.07]
-console.log("Square roots of array", result);
+    for (var num=0; num<maxNums; num++) {
+        debugger;
+        
+        var outputStr = "";
+       
+        for (var factor in factorObj) {
+            // check to see if this num is a multiple of factor
+            if (num % factor == 0) {
+                // if yes, than add the text to output string
+                outputStr += factorObj[factor];
+            }
+        }
+        
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(num.toString() + outputStr)
+    }
+}
 
-// your map results data
-var mapResults = "Your map results here"; 
-// use jQuery to select the element by it ID and set the HTML content
-$("#output").html(mapResults);
+function reportError(str) {
+    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
+
+document.getElementById("submit").addEventListener("click", function() {
+    var max = document.getElementById("max").value;
+    console.log("max:", max)
+    if (! max) {
+        reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    // clear error if there is one
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
+})
+
