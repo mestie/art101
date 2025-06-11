5ad399b134d5b7dac2645ea81c3dff40e742bf6d
mestie <meelthom@ucsc.edu>
2025-06-10 19:12:48 -0700

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,42 +1,32 @@
-// index.js - Lab 8
-// Author: mestie
-// Date: 7.9.2025
+// index.js - Lab 15
+// Author: mestie thomas
+// Date: June 2025
 
+// Set up AJAX request details
+const ajaxObj = {
+  url: "https://yesno.wtf/api", 
+  type: "GET",                  
+  dataType: "json"             
+}
 
-// Function to determine the nation based on the length of the name
-function sortNation(num) {
-    let remainder = num % 4; // Get the remainder when dividing the name length by 4
-    let str = "";
-  
-    // Determine the nation based on the remainder
-    if (remainder == 0) {
-      str = "Fire Nation!";
-    } else if (remainder == 1) {
-      str = "Earth Kingdom!"; 
-    } else if (remainder == 2) {
-      str = "Water Tribe!";
-    } else if (remainder == 3) {
-      str = "Air Nation!";
-    }
-  
-    return str; // Return the determined nation
-  }
-  
-  // Click handler for the button
-  $("#button").click(function () {
-    let name = $("#input").val(); // Get the value entered in the input field
-  
-    // Check if the user entered a name
-    if (name) {
-      console.log("there is data");
-    } else {
-      // Show message if input is empty
-      $("#output").append("Don't forget to enter a name.");
-      return; // Stop further execution if input is missing
-    }
-  
-    let nameLength = name.length; // Get the length of the input name
-    let nation = sortNation(nameLength); // Call function to get the nation based on length
-    $("#output").html("<h1>" + nation + "</h1>"); // Display result on the page
+// When button is clicked
+$("#activate").click(function() {
+  console.log("click");
+
+  // Make AJAX request
+  $.ajax(ajaxObj)
+
+  // If successful
+  .done(function(data){
+    console.log("success");
+    console.log(data);
+    $("#output").text(data.answer); 
+    $("#image").attr("src", data.image).css("display", "block"); 
+  })
+
+  // If failed
+  .fail(function(xhr, status, errorThrown){
+    console.log(errorThrown + " Status:" + status); 
   });
-  
+})
+
